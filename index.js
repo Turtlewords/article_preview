d879c5ec84756017bfeb382dd74252ed28ed4074
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-03-27 18:49:29 -0500

    style desktop + toast two

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const shareIcon = document.getElementById("share-icon");
 const info = document.getElementById("info");
-const toast = document.getElementById("toast")
+const toast = document.getElementById("toast");
+const toastTwo = document.getElementById("toast-two");
 const shareIconToast = document.getElementById("share-icon-toast");
 
 
+let visibleToast = false;
 
 shareIcon.addEventListener("click", () => {
-    info.style.display = "none"
-    toast.style.display = "flex";   
+    let width = screen.width;
+
+    if (width >= 768) {
+        if (!visibleToast) {
+            toastTwo.style.display = "flex"
+            visibleToast = true;
+        } else {
+            toastTwo.style.display = "none"
+            visibleToast = false;
+        }
+    } else {
+        info.style.display = "none"
+        toast.style.display = "flex";
+    }
+   
+       
 })
 
 shareIconToast.addEventListener("click", () => {
-    toast.style.display = "none"
-    info.style.display = "flex";   
+    let width = screen.width;
+    if (width < 768) {
+        toast.style.display = "none"
+        info.style.display = "flex"; 
+    } else {
+        toastTwo.style.display = "none"
+        info.style.display = "flex"; 
+    }
+      
 })
